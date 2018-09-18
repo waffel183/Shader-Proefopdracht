56a14a8063ef3d57e7f963ef069077a655617c63
Koen Sanderse <koensanderse2000@gmail.com>
2018-09-18 10:47:41 +0200

    Added basic Unity project

---
+++ Proefopdracht_Shaders/Assets/Scripts/Goblin.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : MonoBehaviour {
+
+    private Renderer rend;
+    float FatAmount = 0;
+
+    private int _timer = 100;
+
+	// Use this for initialization
+	void Awake () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rend.material.SetFloat("_Amount", FatAmount);
+
+        if(FatAmount < -1 || FatAmount > 1)    //not valid amounts
+        {
+            _timer = -1; //disable timer
+        }
+
+        if (_timer == 0)
+        {
+            FatAmount -= 0.1f;
+            _timer = 100;
+        }
+        else _timer -= 1;
+	}
+    private void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("TRIGGERED!!!");
+        if(col.gameObject.tag == "Food")
+        {
+            FatAmount += 0.5f;
+        }
+    }
+}
---
+++ Proefopdracht_Shaders/Assets/Scripts/GoblinMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinMovement : MonoBehaviour {
+
+    Animator Animator;
+    [SerializeField]
+    //private Animation running;
+    //private Animation Idle;
+    
+
+	// Use this for initialization
+	void Start () {
+        Animator = GetComponent<Animator>();
+	}
+
+    void Movement()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        if(z != 0)
+        {
+            Animator.Play("Run_State");
+        }
+        else
+        {
+            Animator.Play("Ilde_State");
+        }
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Movement();
+	}
+}
